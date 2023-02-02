e1f2ff98bb06185a3941f2cb0b55734e9d0ebf71
Pedro Parro <91163584+pedroparro@users.noreply.github.com>
2023-02-02 17:34:59 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace fv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Clique no botão abaixo";
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnExport.FlatAppearance.BorderSize = 0;
+            this.btnExport.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.Color.Green;
+            this.btnExport.Location = new System.Drawing.Point(12, 63);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(358, 101);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "EXPORTAR";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(382, 222);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AUTOMATIZAÇÃO EXCEL - WORD 2023";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Text.RegularExpressions;
+
+namespace fv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            //create word
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+
+            //carregar documento
+            Microsoft.Office.Interop.Word.Document doc = null;
+            Object fileName = @"C:\Users\pedro.parro\Desktop\Convert\MODELO.docx";
+            Object missing = Type.Missing;
+            for (int i = 0; i < 500; i++)
+            {
+                doc = app.Documents.Open(fileName, missing, missing);
+                app.Selection.Find.ClearFormatting();
+                app.Selection.Find.Replacement.ClearFormatting();
+
+                //read file excel
+                String[] tmp = new string[2];
+                tmp = readExcel(i);
+
+                
+
+                //fill data
+                app.Selection.Find.Execute("<Coluna-A>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[0], 2);
+                app.Selection.Find.Execute("<Coluna-B>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[1], 2);
+                app.Selection.Find.Execute("<Coluna-C>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[2], 2);
+                app.Selection.Find.Execute("<Coluna-D>", missing, missing, missing, missing, missing, missing, missing, missing, formatString(tmp[3]), 2);
+                app.Selection.Find.Execute("<Coluna-E>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[4], 2);
+                app.Selection.Find.Execute("<Coluna-F>", missing, missing, missing, missing, missing, missing, missing, missing, uppercaseString(removerString(tmp[5])), 2);
+                app.Selection.Find.Execute("<Coluna-G>", missing, missing, missing, missing, missing, missing, missing, missing, removeString(tmp[6]), 2);
+
+
+
+                //save file
+                object SaveAsFile = (object)@"C:\Users\pedro.parro\Desktop\Convert\" + tmp[0] + "-" + tmp[2] + ".docx";
+                doc.SaveAs2(SaveAsFile, missing, missing, missing);
+            }
+            //mensagem
+            MessageBox.Show("Arquivo criado com sucesso...", "MENSAGEM", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+
+            doc.Close(false, missing, missing);
+            app.Quit(false, false, false);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+        }
+        //REGEX
+        private static string formatString(string number)
+        {
+            Regex regex = new Regex(@"[^\d]");
+            number = regex.Replace(number, "");
+            //string format = @"#######\-##\.####\.#\.##\.####";
+            string format = @"0000000\-00\.0000\.0\.00\.0000";
+            number = double.Parse(number).ToString(format);
+            return number;
+        }
+
+        //REMOVE STRING GERAL
+        private static string removeString(string str = "")
+        {
+            string result = str.Replace("(", "").Replace(")", "").Replace("GERAL", "").Replace("-", "").Replace("SP", "").Replace("BA", "").Replace("MG", "").Replace("PR", "").Replace("SC", "").Replace("RJ", "");
+            return result;
+
+        }
+
+        //REMOVE STRING VARA
+        private static string removerString(string str = ".")
+        {
+            string result = str.Replace(".", "");
+            return result;
+
+        }
+
+        //UPPERCASE
+        private static string uppercaseString(string upc)
+        {
+            return upc.ToUpper();
+        }
+
+        private string[] readExcel(int index)
+        {
+            string res = @"C:\Users\pedro.parro\Desktop\Convert\PLANILHA.xlsx";
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(res, 0, true, 5, "", "", true);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            index += 2;
+
+            string[] data = new string[7]; //array[id,nome,age]
+            data[0] = xlWorkSheet.get_Range("A" + index.ToString()).Text; //number = TEXT
+            data[1] = xlWorkSheet.get_Range("B" + index.ToString()).Value; //text = VALUE
+            data[2] = xlWorkSheet.get_Range("C" + index.ToString()).Value;
+            data[3] = xlWorkSheet.get_Range("D" + index.ToString()).Text;
+            data[4] = xlWorkSheet.get_Range("E" + index.ToString()).Value;
+            data[5] = xlWorkSheet.get_Range("F" + index.ToString()).Value;
+            data[6] = xlWorkSheet.get_Range("G" + index.ToString()).Value;
+
+            xlWorkBook.Close(false);
+            xlApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlWorkSheet);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlWorkBook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlApp);
+
+            //return data[]
+            return data;
+
+        }
+    }
+}
